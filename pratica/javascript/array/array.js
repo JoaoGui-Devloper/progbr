37f0ffd692725cbf86a449c22c9006ca09d9c538
JoaoGui-Devloper <116917749+JoaoGui-Devloper@users.noreply.github.com>
2023-06-23 12:06:44 -0300

    fiz primeiro ex sobre array

---
+++ pratica/javascript/array/array.js
@@ -0,0 +1,18 @@
+var alunos = ["João","Maria","Natália","José","Carlos"]
+
+// for (var i = 0; i < alunos.length; i++) {
+//     console.log(alunos[i])
+// }
+
+// ---------------------------------------------------outra forma--------------------------------------------------------
+
+// for (var i in alunos) {
+//     console.log(alunos[i])
+// }
+
+// for (var i of alunos) {
+//     console.log(i)
+// }
+
+// in pega o valor do indice do array e o of pega o valor que está no array em si.
+// ambos vão de um em um até o final do array, não precisando fazer um calculo como no primeiro for acima.
