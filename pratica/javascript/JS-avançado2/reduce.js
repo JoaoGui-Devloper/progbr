41b7af3fda93b3ae3e8ae39eaa7c20a7ca82e11d
João Guilherme <116917749+JoaoGui-Devloper@users.noreply.github.com>
2023-10-18 18:16:31 -0300

    revendo reduce

--- pratica/javascript/JS-avançado2/reduce.js
+++ pratica/javascript/JS-avançado2/reduce.js
@@ -14,3 +14,21 @@
 // };
 
 // console.log(alunos.reduce(idadeDosAlunos, 0))
+
+function novoAluno(nome, nota1) {
+    return {nome, nota1}
+}
+
+let alunos = [
+    novoAluno("João", 10),
+    novoAluno("Igor", 10),
+    novoAluno("Igor", 1)
+]
+
+function mediaDosAlunos(total, aluno) {
+    return total + aluno.nota1 / alunos.length
+}
+
+console.log(alunos.reduce(mediaDosAlunos, 0))
+
+// o reduce pega todo o array e transforma em um único resultado.
