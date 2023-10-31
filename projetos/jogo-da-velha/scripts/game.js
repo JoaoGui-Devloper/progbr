17579c20297b70580052f29ec3fe5ed17c7f8ddf
JoaoGui-Devloper <116917749+JoaoGui-Devloper@users.noreply.github.com>
2023-10-31 17:43:56 -0300

    Update game.js

--- projetos/jogo-da-velha/scripts/game.js
+++ projetos/jogo-da-velha/scripts/game.js
@@ -1,14 +1,25 @@
 let board = ["", "", "", "", "", "", "", "", ""];
 let playerTime = 0;
 let symbols = ['O', 'X'];
+let gameOver = false;
 
 function handleMove (position) {
+
+    if (gameOver) {
+        return;
+    };
 
     if (board[position] == '') {
         
     board[position] = symbols[playerTime];
 
+    gameOver = isWin();
+
     playerTime = (playerTime == 0) ? 1 : 0
 
     }
+}
+
+function isWin() {
+    
 }
