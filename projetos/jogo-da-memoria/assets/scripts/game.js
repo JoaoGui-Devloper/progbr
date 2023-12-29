0726925dc5652e8d87e0374bae3f777e0b76c527
João Guilherme <116917749+JoaoGui-Devloper@users.noreply.github.com>
2023-12-29 13:47:35 -0300

    terminei o projeto

--- projetos/jogo-da-memoria/assets/scripts/game.js
+++ projetos/jogo-da-memoria/assets/scripts/game.js
@@ -1,5 +1,9 @@
 let game = {
 
+    lockMode: false,
+    firstCard: null,
+    secondCard: null,
+
     techs: ['bootstrap',
     'css',
     'html',
@@ -13,7 +17,47 @@ let game = {
 
     cards: null,
 
+    setCard: function(id) {
+        let card = this.cards.filter(card=> card.id === id)[0];
 
+        if(card.flipped || this.lockMode) {
+            return false;
+        }
+
+        if(!this.firstCard) {
+            this.firstCard = card;
+            this.firstCard.flipped = true;
+            return true;
+        } else{
+            this.secondCard = card;
+            this.lockMode = true;
+            this.secondCard.flipped = true;
+            return true
+        };
+    },
+
+    checkMatch: function() {
+        if(!this.firstCard || !this.secondCard) {
+            return false;
+        }
+        return this.firstCard.icon === this.secondCard.icon;
+    },
+
+    unflipCards: function () {
+        this.firstCard.flipped = false;
+        this.secondCard.flipped= false;
+        this.clearCards();
+    },
+
+    clearCards: function() {
+        this.firstCard = null;
+        this.secondCard = null;
+        this.lockMode = false;
+    },
+
+    checkGameOver: function() {
+        return this.cards.filter(card => !card.flipped).length === 0;
+    },
 
     createCardsFromTechs: function() {
 
